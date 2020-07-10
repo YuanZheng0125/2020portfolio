6318ef513615b1ccfc56612fbfa34a23f178257a
YuanZheng0125 <yz4929@g.rit.edu>
2020-07-09 17:39:52 -0700

    change project preview and span images

---
+++ dist/js/project.js
@@ -0,0 +1,75 @@
+// slide show
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// fade in when scroll into viewport
+$(function () {
+  // $(document).ready shorthand
+  $(".brief").fadeIn("slow");
+});
+
+$(document).ready(function () {
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function () {
+    /* Check the location of each desired element */
+    $(".fadethisdiv").each(function (i) {
+      var top_of_object = $(this).position().top;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window > top_of_object) {
+        $(this).animate({ opacity: "1" }, 2000);
+      }
+    });
+  });
+});
+
+// Add Back to Top Button
+backbtn = document.getElementById("backtotop");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backbtn.style.display = "block";
+  } else {
+    backbtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
