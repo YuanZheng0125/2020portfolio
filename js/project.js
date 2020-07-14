8fb1242c2af6b5fce2ad82750588e58e2e2a6d6d
YuanZheng0125 <yz4929@g.rit.edu>
2020-07-13 21:26:41 -0700

    Updates

--- js/project.js
+++ js/project.js
@@ -31,6 +31,39 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
+// 2nd-slide show
+var slideTwoIndex = 1;
+showSlidesTwo(slideTwoIndex);
+
+function plusSlidesTwo(n) {
+  showSlidesTwo((slideTwoIndex += n));
+}
+
+function currentSlideTwo(n) {
+  showSlidesTwo((slideTwoIndex = n));
+}
+
+function showSlidesTwo(n) {
+  var j;
+  var slidestwo = document.getElementsByClassName("mySlidesTwo");
+  var dotstwo = document.getElementsByClassName("dotTwo");
+
+  if (n > slidestwo.length) {
+    slideTwoIndex = 1;
+  }
+  if (n < 1) {
+    slideTwoIndex = slidestwo.length;
+  }
+  for (j = 0; j < slidestwo.length; j++) {
+    slidestwo[j].style.display = "none";
+  }
+  for (j = 0; j < dotstwo.length; j++) {
+    dotstwo[j].className = dotstwo[j].className.replace(" active", "");
+  }
+  slidestwo[slideTwoIndex - 1].style.display = "block";
+  dotstwo[slideTwoIndex - 1].className += " active";
+}
+
 // fade in when scroll into viewport
 $(function () {
   // $(document).ready shorthand
@@ -61,7 +94,10 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (
+    document.body.scrollTop > 800 ||
+    document.documentElement.scrollTop > 800
+  ) {
     backbtn.style.display = "block";
   } else {
     backbtn.style.display = "none";
@@ -73,3 +109,34 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// Collapsible Index
+var coll = document.getElementsByClassName("btn-index");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// scroll 800px to show index
+
+indexbtn = document.getElementById("index");
+
+var IndexScrollFunc = function () {
+  var y = window.scrollY;
+  if (y >= 800) {
+    indexbtn.className = "show";
+  } else {
+    indexbtn.className = "hide";
+  }
+};
+
+window.addEventListener("scroll", IndexScrollFunc);
