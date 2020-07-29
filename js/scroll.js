9cf0fcc18021bc597d99bf75a8d930ff94af7486
YuanZheng0125 <yz4929@g.rit.edu>
2020-07-29 00:52:16 -0700

    Updates

--- js/scroll.js
+++ js/scroll.js
@@ -74,3 +74,29 @@ function navActivatorBuilder(navClass, activeClass) {
     }
   };
 }
+
+// horizontal scroll
+function horizontalScroll() {
+  function scrollHorizontally(e) {
+    e.preventDefault();
+    e = window.event || e;
+    var delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+    document.getElementById("horizontal-scroll-wrapper").scrollLeft -=
+      delta * 40; // Multiplied by 40
+  }
+  if (document.getElementById("horizontal-scroll-wrapper").addEventListener) {
+    // IE9, Chrome, Safari, Opera
+    document
+      .getElementById("horizontal-scroll-wrapper")
+      .addEventListener("mousewheel", scrollHorizontally, false);
+    // Firefox
+    document
+      .getElementById("horizontal-scroll-wrapper")
+      .addEventListener("DOMMouseScroll", scrollHorizontally, false);
+  } else {
+    // IE 6/7/8
+    document
+      .getElementById("horizontal-scroll-wrapper")
+      .attachEvent("onmousewheel", scrollHorizontally);
+  }
+}
